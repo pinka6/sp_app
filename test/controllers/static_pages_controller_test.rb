64b5b5d66a92b45529367101e58463174ed49b22
pinka6 <lior_pinkus@hotmail.com>
2015-08-26 14:28:33 +0300

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | SpGroupApp"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | SpGroupApp"
+  end
 end
