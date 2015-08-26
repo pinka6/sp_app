a270608449eab6154b81fa36f2ad98becda91bd7
pinka6 <lior_pinkus@hotmail.com>
2015-08-26 12:47:38 +0300

    Finish layout and routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815172225) do
+ActiveRecord::Schema.define(version: 20150819104717) do
 
   create_table "agents", force: :cascade do |t|
     t.integer  "contact_id"
     t.string   "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "clients", force: :cascade do |t|
